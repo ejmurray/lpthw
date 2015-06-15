27c87d4debf948e8b74642aaa7649ce1c1e555c8
Ernest Murray <ejmurray247@hotmail.com>
2015-06-15 15:40:25 +0100

    added this exercise today
    there is no description for this file

---
+++ ex19a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+__author__ = 'Ernest'
+
+# this is an exercise from the slideshare on lpthw.
+
+a = "How many miles per gallon does your car get? "
+b = "What is the current price of gas, per gallon? (Please use a decimal, like this 2.95 )"
+c = "What is the length of your trip in miles? "
+
+
+mpg = float(raw_input(a))
+price_of_gas = float(raw_input(b))
+distance = float(raw_input(c))
+
+# using float will allow decimal numbers
+
+
+def journey_cost(mpg, price_of_gas, distance):
+    total_fuel = distance / mpg
+    cost = total_fuel * price_of_gas
+    return cost
+
+total = journey_cost(mpg, price_of_gas, distance)
+print "The cost of this journey will be %d pounds." % total
